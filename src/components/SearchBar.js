b0fda10bcfcf58cd4992ac2c8622d907b368b3b9
bijalpatel22 <strange2209@users.noreply.github.com>
2021-01-04 02:33:38 -0800

    Added hooks
    Implemented functions using hooks and validated input text for search.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import newsApi from '../apis/newsApi';
+import NewsList from './NewsList';
 
-class SearchBar extends React.Component {
-	state = {term:''};
+const SearchBar = () => {
+	const [term, setTerm] = useState('');
+	const [debouncedTerm, setDebouncedTerm] = useState('');
+	const [results, setResults] = useState([]);
 
-	onInputChange = (event) => {
-		this.setState({term:event.target.value});
-	};
+	useEffect(() =>{
+		const search = async () => {
+			const response = await newsApi.get('/', {
+				params: {
+					q:debouncedTerm
+				},
+			});
+			setResults(response.data.articles);
+		};
+		if(debouncedTerm && debouncedTerm.trim() !== "") {
+			search();
+		}
+	}, [debouncedTerm]);
 
-	onFormSubmit = event => {
-		event.preventDefault();
-		this.props.onFormSubmit(this.state.term);
-	};
-
-	render() {
-		return (
+	return (
+		<div>
 			<div className="search-bar">
 				<div style={{width:"100%", height:"70px", backgroundColor:"#606060"}}></div>
 				<div className = "ui segment" style={{backgroundColor:"#D0D4D4", padding:"40px", marginTop:"0px"}}>
-					<form className="ui form" onSubmit = {this.onFormSubmit} >
+					<form 
+					 className="ui form" 
+					 onSubmit = {e => {
+					 	e.preventDefault(); 
+					 	setDebouncedTerm(term);
+					 }} 
+					>
 						<div className = "field ui search">
 							<div className="ui icon input">
 								<input 
 									type="text" 
-									value={this.state.term} 
-									onChange = {this.onInputChange}
+									value={term} 
+									onChange = {e => setTerm(e.target.value)}
 									placeholder = "Search Something..."
 								/>
 								<i className="search icon"></i>
@@ -32,9 +47,11 @@ class SearchBar extends React.Component {
 					</form>
 				</div>
 			</div>
-		);
-	}
+			<div style= {{paddingLeft:"70px", paddingTop:"50px"}}>
+				<NewsList news={results}/>
+			</div>
+		</div>
+	);
 }
-
 
 export default SearchBar;
